658c566ab0abf625b6a34bf43394e23387e3c78b
Dan McDermet <dan.mcdermet@vanderbilt.edu>
2018-12-10 10:49:56 -0800

    added General Help

--- src/panels/HelpLogger.java
+++ src/panels/HelpLogger.java
@@ -52,12 +52,12 @@ public class HelpLogger {
     helpFrame.addWindowListener(new Window_MainListener());
 
     String fonttype = "Courier";
-    FontInfo.setTypeColor (fontmap, MsgType.TITLE.toString(),     FontInfo.TextColor.Black, FontInfo.FontType.Bold, 20, fonttype);
+    FontInfo.setTypeColor (fontmap, MsgType.TITLE.toString(),     FontInfo.TextColor.DkVio, FontInfo.FontType.BoldItalic, 20, fonttype);
     FontInfo.setTypeColor (fontmap, MsgType.HEADER1.toString(),   FontInfo.TextColor.Black, FontInfo.FontType.Bold, 18, fonttype);
     FontInfo.setTypeColor (fontmap, MsgType.HEADER2.toString(),   FontInfo.TextColor.Black, FontInfo.FontType.Bold, 16, fonttype);
     FontInfo.setTypeColor (fontmap, MsgType.HEADER3.toString(),   FontInfo.TextColor.Black, FontInfo.FontType.BoldItalic, 15, fonttype);
     FontInfo.setTypeColor (fontmap, MsgType.NORMAL.toString(),    FontInfo.TextColor.Black, FontInfo.FontType.Normal, 14, fonttype);
-    FontInfo.setTypeColor (fontmap, MsgType.EMPHASIS.toString(),  FontInfo.TextColor.Black, FontInfo.FontType.Bold, 14, fonttype);
+    FontInfo.setTypeColor (fontmap, MsgType.EMPHASIS.toString(),  FontInfo.TextColor.DkGrey, FontInfo.FontType.BoldItalic, 14, fonttype);
     FontInfo.setTypeColor (fontmap, MsgType.HYPERTEXT.toString(), FontInfo.TextColor.Blue,  FontInfo.FontType.Bold, 14, fonttype);
 
     // create the text panel and assign it to the logger
@@ -90,46 +90,103 @@ public class HelpLogger {
     // make panel writable, erase current data, then write the selected header
     panel.setEditable(true);
     logger.clear();
-    printTitle(select);
     
     // write the context-specific help messages
     switch(select) {
       case "GENERAL":
-        printParagraph("This line just goes on and on and on and on and on and on and on" +
-                       " and on and on and on and on and on and on and on and on and there" +
-                       " are some ^^highlighted^^ words and some ^^cute little phrases^^ that are" +
-                       " included in this paragraph.");
-        printParagraph("This line just goes on and on and on and on and on and on and on" +
-                       " and on and <<on and on and on>> and on and on and on and on and there" +
-                       " are some ^^highlighted^^ words and some *^cute little phrases^^ that are" +
-                       " <<included>> in this paragraph.");
-        printNormalText("No help for this item yet!");
+        printTitle("General Operation");
+        printParagraph("This program allows the user to instrument and run a Java application using " +
+                       "the ^^danalyzer^^ DSE. It provides an interface for allowing the user to " +
+                       "specify <<Symbolic Parameters>> that the DSE keeps track of in the shadow stack " +
+                       "it maintains that allows the ^^dansolver^^ application to solve. As the program " +
+                       "runs, the DSE builds up a list of constraints for the symbolic parameters selected, " +
+                       "and when the user wants to find solutions for those parameters, he can stop the " +
+                       "execution and run the solver. The <<SOLUTIONS>> tab at the bottom of the screen " +
+                       "will display a list of the constraints that were found for the symbolic parameters " +
+                       "and the solutions (if any) that the solver was able to determine.");
+        printParagraph("The first step is selecting the application to run. Yu do this by selecting the " +
+                       "entry (Load jar file) from the Project menu selection. This will generate the " +
+                       "instrumented jar file that can then be run with symbolic parameters enabled. This " +
+                       "will then enable the entries in the ^^Controls^^ panel that allow you to run the " +
+                       "application and solve any constraints.");
+        printParagraph("The <<Controls>> panel allows you to specify the Main class of the application to " +
+                       "run, and to ^^RUN^^ and ^^STOP^^ the execution of the application. Prior to running " +
+                       "you will need to specify the argument list in the text area to the right of the " +
+                       "STOP button. If the application runs as a web server and takes HTTP-formatted POST " +
+                       "messages, these may be sent by the lines below the RUN and STOP buttons by specifying " +
+                       "the port to send to in the first text window and the message to send in the second, " +
+                       "and then pressing the ^^Post^^ button.");
+        printParagraph("While the application is running, the user may opt to have messages from the " +
+                       "application be displayed in the <<LOG>> tab at the bottom. The user enables these " +
+                       "messages in the <<Configuration>> panel. Note that the more messages are displayed, " +
+                       "the slower the application will run, so take care to only enable those messages that" +
+                       "are essential to be monitored. When logging is enabled, if the CALLS selection " +
+                       "is made it will also keep track of the call structure of the application, which will " +
+                       "be displayed graphically in the <<CALLGRAPH>> tab. This allows the user to see " +
+                       "visually how the methods are called.");
+        printParagraph("Additionally, if the user has run another test application, ^^janalyzer^^ prior to " +
+                       "this, he can, within that program, save the call graph that it generates for the " +
+                       "entire application. It saves it as a JSON-formatted text file, which can then be " +
+                       "loaded into this test program under the Project menu selection (Load JSON Graph), " +
+                       "which will bring up that call graph in the <<JANAGRAPH>> tab at the bottom of the " +
+                       "screen. When the application is run with CALLS logging messages enabled, it will " +
+                       "highlight the methods in this graph as they are traversed to see visually the " +
+                       "methods that are called and those that are not.");
+        printParagraph("To specify a symbolic parameter to define for the application, the user needs to " +
+                       "bring up the method he is interested in in the <<BYTECODE>> viewer tab. He may do " +
+                       "this by either making the method selection in the Bytecode panel and pressing the " +
+                       "^^Get Bytecode^^ button, or by clicking on the method of interest in either the " +
+                       "CALLGRAPH or JANAGRAPH tabs and indicating you want to display the bytecode for " +
+                       "that method. When in the BYTECODE viewer tab, the bytecode for the selected method " +
+                       "is displayed on the left panel and the local parameters found are displayed in the " +
+                       "right panel. Symbolic parameters are selected from one or more of the local " +
+                       "parameters defined for a method. A parameter may be added to the symbolic list " +
+                       "simply by clicking on the entry in the local parameters list, which will present " +
+                       "the information about the parameter and ask whether you wish to add this to the " +
+                       "list of symbolics. Clicking yes will add it. You may alternatively click on the " +
+                       "corresponding LOAD or STORE opcode entry for the desired parameter in the bytecode " +
+                       "panel on the left to get the same panel asking whether you wish to add the selected " +
+                       "entry to the symbolic list.");
+        printParagraph("Entries in the symbolic list may also be edited by clicking on the entry in the " +
+                       "Symbolic Parameters panel at the top and selecting whether you wish to edit or " +
+                       "delete the symbolic parameter. It also allows you to add your own user-defined " +
+                       "constraints for the specified parameter by specifying one or more entries of " +
+                       "EQ, NE, GT, GE, LT or LE to a specific numeric value.");
         break;
       case "CONFIG":
+        printTitle("Configuration Panel");
         printNormalText("No help for this item yet!");
         break;
       case "CONTROLS":
+        printTitle("Control and Bytecode Panels");
         printNormalText("No help for this item yet!");
         break;
       case "SYMBOLICS":
+        printTitle("Symbolics Panel");
         printNormalText("No help for this item yet!");
         break;
       case "SOLUTIONS":
+        printTitle("SOLUTIONS Tab");
         printNormalText("No help for this item yet!");
         break;
       case "BYTECODE":
+        printTitle("BYTECODE Tab");
         printNormalText("No help for this item yet!");
         break;
       case "BYTEFLOW":
+        printTitle("BYTEFLOW Tab");
         printNormalText("No help for this item yet!");
         break;
       case "LOG":
+        printTitle("LOG Tab");
         printNormalText("No help for this item yet!");
         break;
       case "CALLGRAPH":
+        printTitle("CALLGRAPH Tab");
         printNormalText("No help for this item yet!");
         break;
       case "JANAGRAPH":
+        printTitle("JANAGRAPH Tab");
         printNormalText("No help for this item yet!");
         break;
     }
@@ -141,18 +198,80 @@ public class HelpLogger {
   
   private void printTitle(String message) {
     logger.printField(MsgType.HEADER1.toString(), message + Utils.NEWLINE);
+    printLinefeed();
   }
   
   private void printMainHeader(String message) {
     logger.printField(MsgType.HEADER1.toString(), message + Utils.NEWLINE);
+    printLinefeed();
   }
   
   private void printSubHeader(String message) {
     logger.printField(MsgType.HEADER2.toString(), message + Utils.NEWLINE);
+    printLinefeed();
   }
   
   private void printSubSubHeader(String message) {
     logger.printField(MsgType.HEADER3.toString(), message + Utils.NEWLINE);
+    printLinefeed();
+  }
+  
+  private void printNormalText(String message) {
+    logger.printField(MsgType.NORMAL.toString(), message);
+  }
+  
+  private void printHighlightedText(String message) {
+    logger.printField(MsgType.EMPHASIS.toString(), message);
+  }
+  
+  private void printHyperText(String message) {
+    logger.printField(MsgType.HYPERTEXT.toString(), message);
+  }
+  
+  private void printLinefeed() {
+    logger.printField(MsgType.NORMAL.toString(), Utils.NEWLINE);
+  }
+  
+  private void printParagraph(String message) {
+    // setup the metas to find
+    List<MetaData> metalist = new ArrayList<>();
+    metalist.add(new MetaData(MsgType.EMPHASIS, "^^", "^^"));
+    metalist.add(new MetaData(MsgType.HYPERTEXT, "<<", ">>"));
+
+    while (!message.isEmpty()) {
+      // search for any next meta, exit if none
+      boolean valid = false;
+      MetaData selected = null;
+      for (MetaData entry : metalist) {
+        entry.checkForMeta(message);
+        valid = entry.valid ? true : valid;
+        // select the entry that we encounter first in the string
+        if (entry.valid) {
+          if (selected == null || (entry.start < selected.start)) {
+            selected = entry;
+          }
+        }
+      }
+      if (!valid || selected == null || !selected.valid) {
+        break;
+      }
+      
+      // if there is non-meta text first, output it now
+      if (selected.start > 0) {
+        printNormalText(message.substring(0, selected.start));
+        message = message.substring(selected.start + selected.entry.length());
+      }
+
+      // convert and output the meta text
+      String metastr = message.substring(0, selected.end);
+      logger.printField(selected.type.toString(), metastr);
+      message = message.substring(selected.end + selected.exit.length());
+    }
+      
+    // output any remaining non-meta text
+    if (!message.isEmpty()) {
+      printNormalText(message + Utils.NEWLINE + Utils.NEWLINE);
+    }
   }
   
   private class MetaData {
@@ -173,89 +292,24 @@ public class HelpLogger {
     }
     
     public void checkForMeta(String message) {
-      if (valid) {
-        start = message.indexOf(entry);
-        if (start >= 0) {
-          end = message.substring(start + entry.length()).indexOf(exit);
-          if (end > 0) {
-            valid = true;
-            return;
-          }
-        }
+      if (!valid) {
+        return;
+      }
+
+      end = -1;
+      start = message.indexOf(entry);
+      if (start < 0) {
         valid = false;
+        return;
       }
-    }
-  }
-  
-  private void printParagraph(String message) {
-    // setup the metas to find
-    List<MetaData> metalist = new ArrayList<>();
-    metalist.add(new MetaData(MsgType.EMPHASIS, "^^", "^^"));
-    metalist.add(new MetaData(MsgType.HYPERTEXT, "<<", ">>"));
-
-    while (!message.isEmpty()) {
-      // search for any next meta, exit if none
-      boolean valid = false;
-      MetaData selected = null;
-      for (MetaData entry : metalist) {
-        entry.checkForMeta(message);
-        valid = entry.valid ? true : valid;
-        // select the entry that we encounter first in the string
-        if (selected == null || (entry.valid && (entry.start < selected.start))) {
-          selected = entry;
-        }
-      }
-      if (!valid || selected == null || !selected.valid) {
-        break;
-      }
-      
-      // if there is non-meta text first, output it now
-      if (selected.start > 0) {
-        printNormalText(message.substring(0, selected.start));
-        message = message.substring(selected.start + selected.entry.length());
+      end = message.substring(start + entry.length()).indexOf(exit);
+      if (end <= 0) {
+        valid = false;
+        return;
       }
 
-      // convert and output the meta text
-      String metastr = message.substring(0, selected.end);
-      logger.printField(selected.type.toString(), metastr);
-      message = message.substring(selected.end + selected.exit.length());
+      valid = true;
     }
-      
-//      String pattern1 = "^^";
-//      String pattern2 = "^^";
-//
-//      offset = message.indexOf(pattern1);
-//      if (offset < 0) {
-//        break;
-//      }
-//      printNormalText(message.substring(0, offset));
-//      message = message.substring(offset + pattern1.length());
-//      offset = message.indexOf(pattern2);
-//      if (offset < 0) {
-//        message = pattern2 + message;
-//        break;
-//      }
-//      
-//      printHighlightedText(message.substring(0, offset));
-//      message = message.substring(offset + pattern2.length());
-//    }
-    
-    // output any remaining non-meta text
-    if (!message.isEmpty()) {
-      printNormalText(message + Utils.NEWLINE);
-    }
-  }
-  
-  private void printNormalText(String message) {
-    logger.printField(MsgType.NORMAL.toString(), message);
-  }
-  
-  private void printHighlightedText(String message) {
-    logger.printField(MsgType.EMPHASIS.toString(), message);
-  }
-  
-  private void printHyperText(String message) {
-    logger.printField(MsgType.HYPERTEXT.toString(), message);
   }
   
   private class Window_MainListener extends java.awt.event.WindowAdapter {
